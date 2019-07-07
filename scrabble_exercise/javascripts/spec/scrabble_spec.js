645beaeb722a97c6c54594d3f18e4b323d4672cc
Linus Phan <phanlinus@gmail.com>
2019-07-07 15:49:52 -0700

    complete word scrabble Jasmine test exercise

--- scrabble_exercise/javascripts/spec/scrabble_spec.js
+++ scrabble_exercise/javascripts/spec/scrabble_spec.js
@@ -1,25 +1,25 @@
 describe('Scrabble', function() {
-  xit("scores an empty word as zero",function() {
+  it("scores an empty word as zero",function() {
     expect(Scrabble("")).toEqual(0);
   });
 
-  xit("scores a null as zero",function() {
+  it("scores a null as zero",function() {
     expect(Scrabble(null)).toEqual(0);
   });
 
-  xit("scores a very short word",function() {
+  it("scores a very short word",function() {
     expect(Scrabble("a")).toEqual(1);
   });
 
-  xit("scores the word by the number of letters",function() {
+  it("scores the word by the number of letters",function() {
     expect(Scrabble("street")).toEqual(6);
   });
 
-  xit("scores more complicated words with more",function() {
+  it("scores more complicated words with more",function() {
     expect(Scrabble("quirky")).toEqual(22);
   });
 
-  xit("scores case insensitive words",function() {
+  it("scores case insensitive words",function() {
     expect(Scrabble("MULTIBILLIONAIRE")).toEqual(20);
   });
 });

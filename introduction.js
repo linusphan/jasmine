1345e666accd1726103df6aa643cef2f499e9db0
Linus Phan <phanlinus@gmail.com>
2019-07-07 13:52:47 -0700

    use Spies in Jasmine testing

--- introduction.js
+++ introduction.js
@@ -378,4 +378,241 @@ describe('A spy, when configured to call through', function () {
   });
 });
 
+describe('A spy, when configured to fake a return value', function () {
+  var foo, bar, fetchedBar;
+
+  beforeEach(function () {
+    foo = {
+      setBar: function (value) {
+        bar = value;
+      },
+
+      getBar: function () {
+        return bar;
+      },
+    };
+
+    spyOn(foo, 'getBar').and.returnValue(745);
+
+    foo.setBar(123);
+    fetchedBar = foo.getBar();
+  });
+
+  it('tracks that the spy was called', function () {
+    expect(foo.getBar).toHaveBeenCalled();
+  });
+
+  it('should not affect other functions', function () {
+    expect(bar).toEqual(123);
+  });
+
+  it('when called returns the requested value', function () {
+    expect(fetchedBar).toEqual(745);
+  });
+});
+
+describe('A spy, when configured with an alternate implementation', function () {
+  var foo, bar, fetchedBar;
+
+  beforeEach(function () {
+    foo = {
+      setBar: function (value) {
+        bar = value;
+      },
+
+      getBar: function () {
+        return bar;
+      },
+    };
+
+    spyOn(foo, 'getBar').and.callFake(function (arguments, can, be, received) {
+      return 1001;
+    });
+
+    foo.setBar(123);
+    fetchedBar = foo.getBar();
+  });
+
+  it('tracks that the spy was called', function () {
+    expect(foo.getBar).toHaveBeenCalled();
+  });
+
+  it('should not affect other functions', function () {
+    expect(bar).toEqual(123);
+  });
+
+  it('when called returns the requested value', function () {
+    expect(fetchedbar).toEqual(1001);
+  });
+});
+
+describe('A spy, when configured to throw an error', function () {
+  var foo, bar;
+
+  beforeEach(function () {
+    foo = {
+      setBar: function (value) {
+        bar = value;
+      }
+    };
+
+    spyOn(foo, 'setBar').and.throwError('quux');
+  });
+
+  it('throws the value', function () {
+    expect(function () {
+      foo.setBar(123);
+    }).toThrowError('quux');
+  });
+});
+
+describe('A spy', function () {
+  var foo, bar = null;
+
+  beforeEach(function () {
+    foo = {
+      setBar: function (value) {
+        bar = value;
+      },
+    };
+
+    spyOn(foo, 'setBar').and.callThrough();
+  });
+
+  it('can call through and then stub in the same spec', function () {
+    foo.setBar(123);
+    expect(bar).toEqual(123);
+
+    foo.setBar.and.stub();
+    bar = null;
+
+    foo.setBar(123);
+    expect(bar).toBe(null);
+  });
+});
+
+describe('A spy', function () {
+  var foo, bar = null;
+
+  beforeEach(function () {
+    foo = {
+      setBar: function (value) {
+        bar = value;
+      },
+    };
+
+    spyOn(foo, 'setBar');
+  });
+
+  it('tracks if it was called at all', function () {
+    expect(foo.setBar.calls.any()).toEqual(false);
+
+    foo.setBar();
+
+    expect(foo.setBar.calls.any()).toEqual(true);
+  });
+
+  it('tracks the number of times it was called', function () {
+    expect(foo.setBar.calls.count()).toEqual(0);
+
+    foo.setBar();
+    foo.setBar();
+
+    expect(foo.setBar.calls.count()).toEqual(2);
+  });
+
+  it('tracks the arguments of each call', function () {
+    foo.setBar(123);
+    foo.setBar(456, 'baz');
+
+    expect(foo.setBar.calls.argsFor(0)).toEqual([123]);
+    expect(foo.setBar.calls.argsFor(1)).toEqual([456, 'baz']);
+  });
+
+  it('trakcs the arguments of all calls', function () {
+    foo.setBar(123);
+    foo.setBar(456, 'baz');
+
+    expect(foo.setBar.calls.allArgs()).toEqual([[123], [456, 'baz']]);
+  });
+
+  it('can provide the context and arguments to all calls', function () {
+    foo.setBar(123);
+
+    expect(foo.setBar.calls.all()).toEqual([{object: foo, args: [123], returnValue: undefined}]);
+  });
+
+  it('has a shortcut to the most recent call', function () {
+    foo.setBar(123);
+    foo.setBar(456, 'baz');
+
+    expect(foo.setBar.calls.mostRecent()).toEqual({object: foo, args: [456, 'baz'], returnValue: undefined});
+  });
+
+  it('has a shortcut to the first call', function () {
+    foo.setBar(123);
+    foo.setBar(456, 'baz');
+
+    expect(foo.setBar.calls.first()).toEqual({object: foo, args: [123], returnValue: undefined});
+  });
+
+  it('tracks the context', function () {
+    var spy = jasmine.createSpy('spy');
+    var baz = {
+      fn: spy,
+    };
+
+    var quux = {
+      fn: spy,
+    };
+
+    baz.fn(123);
+    quux.fn(456);
+
+    expect(spy.calls.first().object).toBe(baz);
+    expect(spy.calls.mostRecent().object).toBe(quux);
+  });
+
+  it('can be reset', function () {
+    foo.setBar(123);
+    foo.setBar(456, 'baz');
+
+    expect(foo.setBar.calls.any()).toBe(true);
+
+    foo.setBars.calls.reset();
+
+    expect(foo.setBars.calls.any()).toBe(false);
+  });
+});
+
+describe('A spy, when created manually', function () {
+  var whatAmI;
+
+  beforeEach(function () {
+    whatAmI = jasmine.createSpy('whatAmI');
+
+    whatAmI('I', 'am', 'a', 'spy');
+  });
+
+  it('is named, which helps in error reporting', function () {
+    expect(whatAmI.and.identity()).toEqual('whatAmI');
+  });
+
+  it('tracks that the spy was called', function () {
+    expect(whatAmI).toHaveBeenCalled();
+  });
+
+  it('tracks its number of calls', function () {
+    expect(whatAmI.calls.count()).toEqual(1);
+  });
+
+  it('tracks all the arguments of its calls', function () {
+    expect(whatAmI).toHaveBeenCalledWith('I', 'am', 'a', 'spy');
+  });
+
+  it('allows access to the most recent call', function () {
+    expect(whatAmI.calls.mostRecent().args[0]).toEqual('I');
+  });
+});
+
 
